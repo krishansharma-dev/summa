ca5176eec1ce523b8099e54865093aa9467e5e4b
krishansharma-dev <krishansharma@qurilo.com>
2025-09-02 18:03:45 +0530

    fastapi tut

---
+++ README.md
@@ -0,0 +1,6 @@
+python -m venv venv
+venv\Scripts\activate # activate the virtual environment
+pip install "fastapi[standard]"
+
+ pip freeze 
+ fastapi dev main.py
--- main.py
+++ main.py
@@ -1,13 +1,9 @@
+
 from fastapi import FastAPI
-from api.news import app as news_app  # Rename to avoid namespace collision
 
-# Create a new FastAPI instance
-main_app = FastAPI(title="News API Service", description="A service for fetching news data")
+app = FastAPI()
 
-# Mount the news app under the /api prefix
-main_app.mount("/api", news_app)
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
 
-# Root endpoint for the main app
-@main_app.get("/")
-async def root():
-    return {"message": "Welcome to the News API Service"}
