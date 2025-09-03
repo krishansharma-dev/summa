5af17218260629bc6c6120c81d76249221ef696c
krishansharma-dev <krishansharma@qurilo.com>
2025-09-03 18:59:45 +0530

    1:6

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
-@app.get("/login")
+@app.get("/")
 def main():
     return {"welcome": "to FastAPI"}
 
+@app.get("/post")
+def read_post():
+    return {"post": "This is a post"}
+
+@app.post("/createpost")
+def create_post( payload: dict = Body(...) ):
+    print(payload)
+    return {"title": payload['title'], "content": payload['content']
+            ,"author": payload['author'] ,
+            "tags": payload['tags'] ,
+            }
